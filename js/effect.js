77450ecb4147782ab538b8d3ba25ceb55c2bf149
Горьков Владислав <vladeys717@gmail.com>
2024-12-19 17:18:56 +0500

    ref

--- js/effect.js
+++ js/effect.js
@@ -1,46 +1,45 @@
-const MIN_SCALE = 25;
-const MAX_SCALE = 100;
-const STEP = 25;
-const PERCENT = 100;
-
-const zoomOutButton = document.querySelector('.scale__control--smaller');
-const zoomInButton = document.querySelector('.scale__control--bigger');
-const scaleControlValue = document.querySelector('.scale__control--value');
 const imageUploadPreview = document.querySelector('.img-upload__preview img');
 const effectsContainerElement = document.querySelector('.effects__list');
 const sliderContainer = document.querySelector('.img-upload__effect-level');
 const sliderElement = sliderContainer.querySelector('.effect-level__slider');
 const effectValueElement = sliderContainer.querySelector('.effect-level__value');
 
-let filterType = 'none';
-
-const zoomOutImage = () => {
-  let currentScaleValue = parseInt(scaleControlValue.value, 10);
-  if (currentScaleValue > MIN_SCALE) {
-    currentScaleValue -= STEP;
-    scaleControlValue.value = `${currentScaleValue.toString()}%`;
-    imageUploadPreview.style.transform = `scale(${currentScaleValue / PERCENT})`;
+const EffectsParameters = {
+  NONE: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1
+  },
+  CHROME: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1
+  },
+  SEPIA: {
+    MIN: 0,
+    MAX: 1,
+    STEP: 0.1
+  },
+  MARVIN: {
+    MIN: 0,
+    MAX: 100,
+    STEP: 1
+  },
+  PHOBOS: {
+    MIN: 0,
+    MAX: 3,
+    STEP: 0.1
+  },
+  HEAT: {
+    MIN: 1,
+    MAX: 3,
+    STEP: 0.1
   }
 };
 
-const zoomInImage = () => {
-  let currentScaleValue = parseInt(scaleControlValue.value, 10);
-  if (currentScaleValue < MAX_SCALE) {
-    currentScaleValue += STEP;
-    scaleControlValue.value = `${currentScaleValue.toString()}%`;
-    imageUploadPreview.style.transform = `scale(${currentScaleValue / PERCENT})`;
-  }
-};
-
-const addEventListenerToScaleElemets = () => {
-  zoomOutButton.addEventListener('click', zoomOutImage);
-  zoomInButton.addEventListener('click', zoomInImage);
-};
-
-const removeEventListenerFromScaleElemets = () => {
-  zoomOutButton.removeEventListener('click', zoomOutImage);
-  zoomInButton.removeEventListener('click', zoomInImage);
-};
+let currentFilterType;
+let currentFilterClass;
+let currentEffectParameters;
 
 const getEffectParameters = (min, max, step, funcTo, funcFrom) => ({
   range: {
@@ -56,55 +55,58 @@ const getEffectParameters = (min, max, step, funcTo, funcFrom) => ({
   }
 });
 
+const changeFilterParameters = (effectClass, effectType, effectParameters) => {
+  if (effectType === 'none') {
+    sliderContainer.setAttribute('hidden', true);
+  } else {
+    sliderContainer.removeAttribute('hidden');
+  }
+  currentFilterClass = effectClass;
+  currentFilterType = effectType;
+  currentEffectParameters = effectParameters;
+};
+
 const changeFilter = (currentFilterID) => {
-  let currentFilterClass;
-  let effectParameters;
   switch (currentFilterID) {
     case 'effect-none':
-      sliderContainer.setAttribute('hidden', true);
-      currentFilterClass = 'effects__preview--none';
-      filterType = 'none';
-      effectParameters = getEffectParameters(0, 1, 0.1, (value) => value, (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--none', 'none',
+        getEffectParameters(EffectsParameters.NONE.MIN, EffectsParameters.NONE.MAX, EffectsParameters.NONE.STEP,
+          (value) => value, (value) => parseFloat(value)));
       break;
 
     case 'effect-chrome':
-      sliderContainer.removeAttribute('hidden');
-      currentFilterClass = 'effects__preview--chrome';
-      filterType = 'grayscale';
-      effectParameters = getEffectParameters(0, 1, 0.1, (value) => value.toFixed(1), (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--chrome', 'grayscale',
+        getEffectParameters(EffectsParameters.CHROME.MIN, EffectsParameters.CHROME.MAX, EffectsParameters.CHROME.STEP,
+          (value) => value.toFixed(1), (value) => parseFloat(value)));
       break;
 
     case 'effect-sepia':
-      sliderContainer.removeAttribute('hidden');
-      currentFilterClass = 'effects__preview--sepia';
-      filterType = 'sepia';
-      effectParameters = getEffectParameters(0, 1, 0.1, (value) => value.toFixed(1), (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--sepia', 'sepia',
+        getEffectParameters(EffectsParameters.SEPIA.MIN, EffectsParameters.SEPIA.MAX, EffectsParameters.SEPIA.STEP,
+          (value) => value.toFixed(1), (value) => parseFloat(value)));
       break;
 
     case 'effect-marvin':
-      sliderContainer.removeAttribute('hidden');
-      currentFilterClass = 'effects__preview--marvin';
-      filterType = 'invert';
-      effectParameters = getEffectParameters(0, 100, 1, (value) => `${value}%`, (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--marvin', 'invert',
+        getEffectParameters(EffectsParameters.MARVIN.MIN, EffectsParameters.MARVIN.MAX, EffectsParameters.MARVIN.STEP,
+          (value) => `${value}%`, (value) => parseFloat(value)));
       break;
 
     case 'effect-phobos':
-      sliderContainer.removeAttribute('hidden');
-      currentFilterClass = 'effects__preview--phobos';
-      filterType = 'blur';
-      effectParameters = getEffectParameters(0, 3, 0.1, (value) => `${value.toFixed(1)}px`, (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--phobos', 'blur',
+        getEffectParameters(EffectsParameters.PHOBOS.MIN, EffectsParameters.PHOBOS.MAX, EffectsParameters.PHOBOS.STEP,
+          (value) => `${value.toFixed(1)}px`, (value) => parseFloat(value)));
       break;
 
     case 'effect-heat':
-      sliderContainer.removeAttribute('hidden');
-      currentFilterClass = 'effects__preview--heat';
-      filterType = 'brightness';
-      effectParameters = getEffectParameters(1, 3, 0.1, (value) => value.toFixed(1), (value) => parseFloat(value));
+      changeFilterParameters('effects__preview--heat', 'brightness',
+        getEffectParameters(EffectsParameters.HEAT.MIN, EffectsParameters.HEAT.MAX, EffectsParameters.HEAT.STEP,
+          (value) => value.toFixed(1), (value) => parseFloat(value)));
       break;
   }
   imageUploadPreview.className = '';
   imageUploadPreview.classList.add(currentFilterClass);
-  sliderElement.noUiSlider.updateOptions(effectParameters);
+  sliderElement.noUiSlider.updateOptions(currentEffectParameters);
 };
 
 const onFilterChange = (evt) => {
@@ -115,14 +117,14 @@ const onFilterChange = (evt) => {
 
 const addFilter = () => {
   effectValueElement.value = 1;
-  filterType = 'none';
+  currentFilterType = 'none';
   noUiSlider.create(sliderElement, getEffectParameters(0, 1, 0.1, (value) => value, (value) => parseFloat(value)));
   sliderContainer.setAttribute('hidden', true);
   effectsContainerElement.addEventListener('change', onFilterChange);
 
   sliderElement.noUiSlider.on('update', () => {
     effectValueElement.value = parseFloat(sliderElement.noUiSlider.get());
-    imageUploadPreview.style.filter = (filterType !== 'none') ? `${filterType}(${sliderElement.noUiSlider.get()})` : '';
+    imageUploadPreview.style.filter = (currentFilterType !== 'none') ? `${currentFilterType}(${sliderElement.noUiSlider.get()})` : '';
   });
 };
 
@@ -134,4 +136,4 @@ const removeFilter = () => {
   sliderElement.noUiSlider.destroy();
 };
 
-export {addEventListenerToScaleElemets, removeEventListenerFromScaleElemets, addFilter, removeFilter };
+export { addFilter, removeFilter };
